bfe6cca320efc4b9f2a1194fc4db6344f7906390
AsmaArrak <asmaarrak11@gmail.com>
2023-12-06 18:24:46 +0100

    Initial commit

---
+++ src/main/java/com/poly/gestionproduitg4/controlleur/SecurityController.java
@@ -0,0 +1,14 @@
+package com.poly.gestionproduitg4.controlleur;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecurityController {
+
+    @GetMapping("/errorPage")
+    public String errorPage() {
+        return "errorpage";
+    }
+}
